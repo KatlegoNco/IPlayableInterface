1645bca72fa6fe066112868f4723addc393f79c2
Grealish10k <katlegoncongwane20@gmail.com>
2024-04-23 14:06:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public interface IPlayable
+{
+    void Play();
+}
+
+public class Guitar : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Guitar is now Playing");
+    }
+}
+
+public class Piano : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Piano is now playing");
+    }
+    public static void Main(string[] args)
+    {
+        // Create instances of Guitar and Piano
+        IPlayable guitar = new Guitar();
+        IPlayable piano = new Piano();
+
+        guitar.Play();
+        piano.Play();
+    }
+}
